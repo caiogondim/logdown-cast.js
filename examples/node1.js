9f503c941decb52b1fcf294183124a10d0008ecc
Caio Gondim <me@caiogondim.com>
2018-03-14 18:51:02 -0400

    Support for more logdown methods

--- examples/node1.js
+++ examples/node1.js
@@ -3,4 +3,6 @@ const logdownCast = require('../lib')
 const debug = logdown('foo')
 logdownCast.publish(debug)
 
-setInterval(() => debug.log('hi', Date.now()), 1000)
+setInterval(() => debug.log('hi', Date.now()), 4000)
+setInterval(() => debug.info('hi', Date.now()), 4000)
+setInterval(() => debug.warn('hi', Date.now()), 4000)
